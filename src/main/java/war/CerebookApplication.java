bf5f90652923d19752ab6d13c5cd7374c59e61cb
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-27 10:39:52 +0200

    exclude to spring boot application

--- src/main/java/war/CerebookApplication.java
+++ src/main/java/war/CerebookApplication.java
@@ -2,8 +2,9 @@ package war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CerebookApplication {
 
 	public static void main(String[] args) {
