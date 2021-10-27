a318506f14c1f2a97226d4036309ff61b562b143
Nicolas Rousset <nrousset@gmail.com>
2021-10-27 13:55:14 +0200

    Checking .env file exists in env_spring_boot_run.sh

--- env_spring_boot_run.sh
+++ env_spring_boot_run.sh
@@ -1,1 +1,11 @@
-env $(cat .env) mvn spring-boot:run 
+#!/bin/bash
+if [ -e .env ]
+then
+    echo "Found .env file with "$(wc -l .env)" lines"
+else
+    echo "You must define a .env file !!"
+    exit 1
+fi
+
+env $(cat .env) mvn spring-boot:run
+
