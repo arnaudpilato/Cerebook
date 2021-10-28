e2855fb733c9fa4862471ea74ac54de46149065d
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-28 19:17:13 +0200

    fix directory

--- src/main/java/wcs/cerebook/CerebookApplication.java
+++ src/main/java/wcs/cerebook/CerebookApplication.java
@@ -1,4 +1,4 @@
-package war;
+package wcs.cerebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/wcs/cerebook/CerebookApplicationTests.java
+++ src/test/java/wcs/cerebook/CerebookApplicationTests.java
@@ -1,4 +1,4 @@
-package war;
+package wcs.cerebook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
