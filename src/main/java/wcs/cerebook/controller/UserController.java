568b25bec1c7c6fc9937e75f337f809147f38693
HOARAU <Mathieuhoarau@laposte.net>
2021-11-22 10:10:43 +0100

    fixed autowired

--- src/main/java/wcs/cerebook/controller/UserController.java
+++ src/main/java/wcs/cerebook/controller/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
     @Autowired
     private GeocodeService geocodeService;
 
+    @Autowired
     private CerebookUserService service;
 
     @GetMapping("/")
@@ -97,5 +98,4 @@ public class UserController {
 
         return "/cerebookUser/users";
     }
-
 }
