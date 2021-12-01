fca46eabee566a3903238ea88344529ff92c0a10
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-12-01 11:27:20 +0100

    change css video

--- src/main/java/wcs/cerebook/repository/VideoRepository.java
+++ src/main/java/wcs/cerebook/repository/VideoRepository.java
@@ -1,9 +1,16 @@
 package wcs.cerebook.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import wcs.cerebook.entity.CerebookPicture;
 import wcs.cerebook.entity.CerebookVideo;
+
+import java.util.List;
 
 @Repository
 public interface VideoRepository extends JpaRepository<CerebookVideo, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM cerebook_video v WHERE v.user_id = :userId ORDER BY id DESC LIMIT 6")
+    List<CerebookVideo> lastVideo(@Param("userId") Long userId);
 }
