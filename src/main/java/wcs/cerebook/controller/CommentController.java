e5724b09e162117e418840b1f947a1c3d1eb9cbc
HOARAU <Mathieuhoarau@laposte.net>
2021-12-02 13:00:33 +0100

    comment on event

--- src/main/java/wcs/cerebook/controller/CommentController.java
+++ src/main/java/wcs/cerebook/controller/CommentController.java
@@ -3,15 +3,14 @@ package wcs.cerebook.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import wcs.cerebook.controller.exception.illegalArgumentException;
 import wcs.cerebook.entity.CerebookComment;
+import wcs.cerebook.entity.CerebookEvent;
 import wcs.cerebook.entity.CerebookPost;
 import wcs.cerebook.entity.CerebookUser;
 import wcs.cerebook.repository.CommentRepository;
+import wcs.cerebook.repository.EventRepository;
 import wcs.cerebook.repository.PostRepository;
 import wcs.cerebook.repository.UserRepository;
 
@@ -29,43 +28,77 @@ public class CommentController {
     @Autowired
     private PostRepository postRepository;
     @Autowired
+    private EventRepository eventRepository;
+    @Autowired
     private UserRepository userRepository;
+
     @GetMapping("/addComment/{postId}")
-    public String addComment(@PathVariable("postId") Long postId, Model model,Principal principal) {
+    public String addComment(@PathVariable("postId") Long postId, Model model, Principal principal) {
         CerebookPost cerebookPost = postRepository.getById(postId);
         CerebookComment cerebookComment = new CerebookComment();
-        String username = principal.getName();
-        CerebookUser user = userRepository.getCerebookUserByUsername(username);
-        model.addAttribute("user",user);
-        cerebookComment.setCerebookUser(user);
+        CerebookUser user = userRepository.getCerebookUserByUsername(principal.getName());
         model.addAttribute("comment", cerebookComment);
-        model.addAttribute("post" ,cerebookPost );
-        cerebookComment.setCreatedAt(new Date());
-        model.addAttribute("time",new Date());
+        model.addAttribute("post", cerebookPost);
+        model.addAttribute("time", new Date());
         return "cerebookComment/addComment";
     }
+
     //save  comment
     @RequestMapping("/savecomment")
-    public String saveComment(CerebookPost cerebookPost,CerebookComment cerebookComment,Principal principal) {
-        String username = principal.getName();
-        CerebookUser user = userRepository.getCerebookUserByUsername(username);
+    public String saveComment(CerebookPost cerebookPost, @ModelAttribute CerebookComment cerebookComment, Principal principal) {
+        CerebookUser user = userRepository.getCerebookUserByUsername(principal.getName());
         cerebookComment.setCerebookPost(cerebookPost);
         cerebookComment.setCerebookUser(user);
+        cerebookComment.setCreatedAt(new Date());
         commentRepository.save(cerebookComment);
         return "redirect:/allPosts";
     }
 
     @GetMapping("/listComment/{postid}")
-    public String showComment(@PathVariable("postid") Long postid,Model model,Principal principal){
-        String username = principal.getName();
+    public String showComment(@PathVariable("postid") Long postid, Model model, Principal principal) {
         List<CerebookUser> user = userRepository.findAll();
         List<CerebookComment> comments = postRepository.getById(postid).getComments();
         List<CerebookPost> post = postRepository.findAll();
         model.addAttribute("listComment", comments);
-        model.addAttribute("user",user);
+        model.addAttribute("user", user);
 
-    model.addAttribute("post",post);
+        model.addAttribute("post", post);
 
         return "cerebookComment/listComments";
+    }
+
+
+    @GetMapping("/addEventComment/{eventid}")
+    public String addEventComment(@PathVariable("eventid") Long eventid, Model model, Principal principal) {
+        CerebookEvent cerebookEvent = eventRepository.getById(eventid);
+        CerebookComment cerebookComment = new CerebookComment();
+        CerebookUser user = userRepository.getCerebookUserByUsername(principal.getName());
+        model.addAttribute("comment", cerebookComment);
+        model.addAttribute("event", cerebookEvent);
+        return "cerebookComment/addEventComment";
+    }
+
+    //save  comment
+    @RequestMapping("/saveEventComment")
+    public String saveEventComment(CerebookEvent cerebookEvent, @ModelAttribute CerebookComment cerebookComment, Principal principal) {
+        CerebookUser user = userRepository.getCerebookUserByUsername(principal.getName());
+        cerebookComment.setCerebookEvent(cerebookEvent);
+        cerebookComment.setCerebookUser(user);
+        cerebookComment.setCreatedAt(new Date());
+        commentRepository.save(cerebookComment);
+        return "redirect:/listEventComment/"+cerebookEvent.getId();
+    }
+
+    @GetMapping("/listEventComment/{eventid}")
+    public String showEventComment(@PathVariable("eventid") Long eventid, Model model, Principal principal) {
+        List<CerebookUser> user = userRepository.findAll();
+        List<CerebookComment> comments = eventRepository.getById(eventid).getComments();
+        CerebookEvent event = eventRepository.getById(eventid);
+        model.addAttribute("listComment", comments);
+        model.addAttribute("user", user);
+
+        model.addAttribute("event", event);
+
+        return "cerebookComment/listEventComments";
     }
 }
