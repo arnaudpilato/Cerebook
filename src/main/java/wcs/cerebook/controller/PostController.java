04d64d43c2ecfd9006bd794926a17feddba56fc6
mister45 <misterdev45@gmail.com>
2021-12-09 21:46:53 +0100

    ajout image dans post et mise en forme tweet

--- src/main/java/wcs/cerebook/controller/PostController.java
+++ src/main/java/wcs/cerebook/controller/PostController.java
@@ -6,19 +6,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import twitter4j.*;
 import wcs.cerebook.controller.exception.illegalArgumentException;
+import wcs.cerebook.entity.CerebookEvent;
 import wcs.cerebook.entity.CerebookPost;
 import wcs.cerebook.entity.CerebookPostLike;
 import wcs.cerebook.entity.CerebookUser;
 import wcs.cerebook.repository.PostLikeRepository;
 import wcs.cerebook.repository.PostRepository;
 import wcs.cerebook.repository.UserRepository;
+import wcs.cerebook.services.MediaService;
 
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import java.util.Date;
@@ -38,6 +45,8 @@ public class PostController {
     private PostLikeRepository likeRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MediaService mediaService;
     @GetMapping("/postCreate")
     public String addPost(Principal principal, Model model) {
         String username = principal.getName();
@@ -57,17 +66,49 @@ public class PostController {
 
     //save post
     @RequestMapping("/save")
-    public String savePost(Principal principal, CerebookPost cerebookPost,CerebookPostLike cerebookPostLike) {
+    public String savePost(Principal principal,@ModelAttribute() CerebookPost cerebookPost,CerebookPostLike cerebookPostLike,@RequestParam(value = "picture") MultipartFile picture) throws IOException {
         // save post to database
         String username = principal.getName();
         CerebookUser user = userRepository.getCerebookUserByUsername(username);
         cerebookPost.setCerebookUser(user);
-        repository.save(cerebookPost);
+
+        if (!picture.isEmpty()) {
+            if (cerebookPost.getId() != null) {
+                if (!picture.isEmpty()) {
+                    saveImage(cerebookPost, principal, picture);
+                } else {
+                    cerebookPost.setImage(repository.getById(cerebookPost.getId()).getImage());
+                }
+            } else {
+                saveImage(cerebookPost, principal, picture);
+            }
+        }
+        String filename = "static/css/data/" + picture.getOriginalFilename();
+
+        try {
+            mediaService.uploadPostImage(
+                    filename,
+                    picture.getInputStream(),
+                    picture.getSize(),
+                    user,
+                    cerebookPost
+            );
+        } catch (IOException e) {
+/*
+            redirectAttributes.addAttribute("errorMessage", e.getMessage());
+*/
+        }
         cerebookPostLike.setCerebookPost(cerebookPost);
         cerebookPostLike.setLiked(true);
         cerebookPostLike.setCerebookUser(user);
         likeRepository.save(cerebookPostLike);
+        Long postId = cerebookPost.getId();
+
         return "redirect:/profil";
+    }
+    private void saveImage(@ModelAttribute CerebookPost cerebookPost, Principal principal, @RequestParam("picture") MultipartFile image) throws IOException {
+        Files.copy(image.getInputStream(), Paths.get("src/main/resources/public/static/css/data/" + principal.getName() + "_image_" + image.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+        cerebookPost.setImage("/static/css/data/" + principal.getName() + "_image_" + image.getOriginalFilename());
     }
     // list all posts
     @RequestMapping("/posts")
